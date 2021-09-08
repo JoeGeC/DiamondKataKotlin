fc2510093126f1882b414e40b6b67cd6a9c0ebba
Joe Barker <joe.barker@mkodo.com>
2021-09-08 17:06:20 +0100

    Refactor

--- src/main/kotlin/Diamond.kt
+++ src/main/kotlin/Diamond.kt
@@ -1,29 +1,38 @@
 class Diamond {
     fun convert(letter: String): String {
-        val b = "B B"
-        val c = "C   C"
-        val d = "D     D"
         if (letter == "D") {
             return "   A\n" +
-                    "  $b\n" +
-                    " $c\n" +
-                    "$d\n" +
-                    " $c\n" +
-                    "  $b\n" +
+                    "  ${createBodyForB()}\n" +
+                    " ${createBodyForC()}\n" +
+                    "${createBodyForD()}\n" +
+                    " ${createBodyForC()}\n" +
+                    "  ${createBodyForB()}\n" +
                     "   A\n"
         }
         if (letter == "C") {
             return "  A\n" +
-                    " $b\n" +
-                    "$c\n" +
-                    " $b\n" +
+                    " ${createBodyForB()}\n" +
+                    "${createBodyForC()}\n" +
+                    " ${createBodyForB()}\n" +
                     "  A\n"
         }
         if (letter == "B") {
             return " A\n" +
-                    "$b\n" +
+                    "${createBodyForB()}\n" +
                     " A\n"
         }
         return "A"
+    }
+
+    private fun createBodyForB(): String {
+        return "B B"
+    }
+
+    private fun createBodyForC(): String {
+        return "C   C"
+    }
+
+    private fun createBodyForD(): String {
+        return "D     D"
     }
 }
