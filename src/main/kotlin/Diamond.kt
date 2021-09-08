cb10e91f1d40101e597b4c2f12332923cf1f10e9
Joe Barker <joe.barker@mkodo.com>
2021-09-08 17:17:42 +0100

    Refactor

--- src/main/kotlin/Diamond.kt
+++ src/main/kotlin/Diamond.kt
@@ -2,39 +2,29 @@ class Diamond {
     fun convert(letter: String): String {
         if (letter == "D") {
             return "   A\n" +
-                    "  ${createBodyForB("B")}\n" +
-                    " ${createBodyForC("C")}\n" +
-                    "${createBodyForD("D")}\n" +
-                    " ${createBodyForC("C")}\n" +
-                    "  ${createBodyForB("B")}\n" +
+                    "  ${createBodyFor("B")}\n" +
+                    " ${createBodyFor("C")}\n" +
+                    "${createBodyFor("D")}\n" +
+                    " ${createBodyFor("C")}\n" +
+                    "  ${createBodyFor("B")}\n" +
                     "   A\n"
         }
         if (letter == "C") {
             return "  A\n" +
-                    " ${createBodyForB("B")}\n" +
-                    "${createBodyForC("C")}\n" +
-                    " ${createBodyForB("B")}\n" +
+                    " ${createBodyFor("B")}\n" +
+                    "${createBodyFor("C")}\n" +
+                    " ${createBodyFor("B")}\n" +
                     "  A\n"
         }
         if (letter == "B") {
             return " A\n" +
-                    "${createBodyForB("B")}\n" +
+                    "${createBodyFor("B")}\n" +
                     " A\n"
         }
         return "A"
     }
 
-    private fun createBodyForB(char: String): String {
-        val space = " ".repeat(spacesFor(char))
-        return "$char${space}$char"
-    }
-
-    private fun createBodyForC(char: String): String {
-        val space = " ".repeat(spacesFor(char))
-        return "$char${space}$char"
-    }
-
-    private fun createBodyForD(char: String): String {
+    private fun createBodyFor(char: String): String {
         val space = " ".repeat(spacesFor(char))
         return "$char${space}$char"
     }
