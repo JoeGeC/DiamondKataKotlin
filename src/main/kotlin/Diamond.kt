70426efb819d5836236c77a04191218129ead413
Joe Barker <joe.barker@mkodo.com>
2021-09-08 17:03:52 +0100

    Refactor

--- src/main/kotlin/Diamond.kt
+++ src/main/kotlin/Diamond.kt
@@ -1,24 +1,27 @@
 class Diamond {
     fun convert(letter: String): String {
+        val b = "B B"
+        val c = "C   C"
+        val d = "D     D"
         if (letter == "D") {
             return "   A\n" +
-                    "  B B\n" +
-                    " C   C\n" +
-                    "D     D\n" +
-                    " C   C\n" +
-                    "  B B\n" +
+                    "  $b\n" +
+                    " $c\n" +
+                    "$d\n" +
+                    " $c\n" +
+                    "  $b\n" +
                     "   A\n"
         }
         if (letter == "C") {
             return "  A\n" +
-                    " B B\n" +
-                    "C   C\n" +
-                    " B B\n" +
+                    " $b\n" +
+                    "$c\n" +
+                    " $b\n" +
                     "  A\n"
         }
         if (letter == "B") {
             return " A\n" +
-                    "B B\n" +
+                    "$b\n" +
                     " A\n"
         }
         return "A"
