768c1e4027e7baafd9df0567a120b63871bd9802
Joe Barker <joe.barker@mkodo.com>
2021-09-09 17:50:31 +0100

    Refactor range

--- src/main/kotlin/Diamond.kt
+++ src/main/kotlin/Diamond.kt
@@ -2,19 +2,14 @@ class Diamond {
     private val chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
     fun convert(letter: String): String {
-        return createTopOfDiamond(letter) + createBottomOfDiamond(letter)
+        return createDiamondHalf(letter, true) + createDiamondHalf(letter, false)
     }
 
-    private fun createTopOfDiamond(letter: String): String {
+    private fun createDiamondHalf(letter: String, top: Boolean): String {
         var result = ""
-        for (i in 0..chars.indexOf(letter))
-            result += "${createBodyFor(chars[i], letter)}\n"
-        return result
-    }
-
-    private fun createBottomOfDiamond(letter: String): String {
-        var result = ""
-        for (i in chars.indexOf(letter) - 1 downTo 0)
+        val range = if(top) 0..chars.indexOf(letter)
+        else chars.indexOf(letter) - 1 downTo 0
+        for (i in range)
             result += "${createBodyFor(chars[i], letter)}\n"
         return result
     }
