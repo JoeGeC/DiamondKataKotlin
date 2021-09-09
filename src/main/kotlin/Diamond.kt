1842390acad66d8d1a551bec40faa0907533ee0d
Joe Barker <joe.barker@mkodo.com>
2021-09-09 11:07:33 +0100

    Refactor

--- src/main/kotlin/Diamond.kt
+++ src/main/kotlin/Diamond.kt
@@ -4,38 +4,39 @@ class Diamond {
     fun convert(letter: String): String {
         if (letter == "D") {
             return "   A\n" +
-                    "  ${createBodyFor("B")}\n" +
-                    " ${createBodyFor("C")}\n" +
-                    "${createBodyFor("D")}\n" +
-                    " ${createBodyFor("C")}\n" +
-                    "  ${createBodyFor("B")}\n" +
+                    "${createBodyFor("B", letter)}\n" +
+                    "${createBodyFor("C", letter)}\n" +
+                    "${createBodyFor("D", letter)}\n" +
+                    "${createBodyFor("C", letter)}\n" +
+                    "${createBodyFor("B", letter)}\n" +
                     "   A\n"
         }
         if (letter == "C") {
             return "  A\n" +
-                    " ${createBodyFor("B")}\n" +
-                    "${createBodyFor("C")}\n" +
-                    " ${createBodyFor("B")}\n" +
+                    "${createBodyFor("B", letter)}\n" +
+                    "${createBodyFor("C", letter)}\n" +
+                    "${createBodyFor("B", letter)}\n" +
                     "  A\n"
         }
         if (letter == "B") {
             return " A\n" +
-                    "${createBodyFor("B")}\n" +
+                    "${createBodyFor("B", letter)}\n" +
                     " A\n"
         }
         return "A"
     }
 
-    private fun createBodyFor(char: String): String {
-        val space = " ".repeat(middleSpacesFor(char))
-        return "$char${space}$char"
+    private fun createBodyFor(char: String, letter: String): String {
+        val middleSpaces = " ".repeat(middleSpacesFor(char))
+        val startingSpaces = " ".repeat(startingSpacesFor(char, letter))
+        return "${startingSpaces}$char${middleSpaces}$char"
     }
 
     private fun middleSpacesFor(char: String) : Int {
         return chars.indexOf(char) * 2 - 1
     }
 
-//    private fun startingSpacesFor(char: String, letter: String) : Int {
-//        return
-//    }
+    private fun startingSpacesFor(char: String, letter: String) : Int {
+        return chars.indexOf(letter) - chars.indexOf(char)
+    }
 }
